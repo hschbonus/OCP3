6b7c3e6bdcfbd7cdfd0d8f215d348549fdfb1de8
Hervé Schmidt <h.schmidt@hotmail.fr>
2025-04-15 16:05:18 +0200

    initial + affichage gallery via API

---
+++ FrontEnd/travaux.js
@@ -0,0 +1,25 @@
+// Appelle l'API pour récupérer les travaux
+const reponse = await fetch('http://localhost:5678/api/works');
+
+// Convertit la réponse en objet JavaScript
+const travaux = await reponse.json();
+
+// Récupération de la section "gallery"
+const sectionGallery = document.querySelector(".gallery");
+
+// Afficher les travaux provenant de l'API
+for (let i = 0; i < travaux.length; i++) {
+    const travauxElement = travaux[i];
+    
+    // Création d’une balise dédiée à un travaux
+    const travauxFigure = document.createElement("figure");
+    travauxFigure.dataset.id = travaux[i].id;
+    
+    // Création des balises
+    const travauxImage = document.createElement("img");
+    travauxImage.src = travauxElement.imageUrl;
+
+    // Ajout du code dans la section "gallery"
+    sectionGallery.appendChild(travauxImage);
+}
+
