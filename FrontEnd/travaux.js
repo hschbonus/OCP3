595fd031b5875ad79e03d39fea2b89f1223e4bc6
Hervé Schmidt <h.schmidt@hotmail.fr>
2025-04-17 10:16:47 +0200

    filtres dynamiques non opérationnelms

--- FrontEnd/travaux.js
+++ FrontEnd/travaux.js
@@ -7,7 +7,34 @@ const travaux = await reponse.json();
 // Récupération de la section "gallery"
 const sectionGallery = document.querySelector(".gallery");
 
-// Afficher les travaux provenant de l'API
+// Récupération des catégories
+const categories = travaux.map(travaux => travaux.category.name);
+
+// Suppression des doublons
+const categoriesSet = [...new Set(categories)];
+console.log(categoriesSet);
+
+// Affichage des catégories en dynamique
+for (let c = 0; c < categoriesSet.length; c++) {
+
+    const sectionCategories = document.querySelector(".categories");
+    const categoriesElement = document.createElement("label");
+    const categoriesInput = document.createElement("input");
+    categoriesInput.type = "radio";
+    categoriesInput.name = "cat";
+    categoriesInput.id = "toggle";
+    categoriesInput.hidden = true;
+
+    const span = document.createElement("span");
+    span.className = "btn";
+    span.textContent = categoriesSet[c];
+
+    categoriesElement.appendChild(categoriesInput);
+    categoriesElement.appendChild(span);
+    sectionCategories.appendChild(categoriesElement);
+}
+
+// Afficher les travaux en dynamique
 for (let i = 0; i < travaux.length; i++) {
     const travauxElement = travaux[i];
     
