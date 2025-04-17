e5e7696aa823dd1edb2a02de9270a9adb6e42515
Hervé Schmidt <h.schmidt@hotmail.fr>
2025-04-17 13:45:06 +0200

    filtres dynamiques opérationnels

--- FrontEnd/travaux.js
+++ FrontEnd/travaux.js
@@ -12,9 +12,8 @@ const categories = travaux.map(travaux => travaux.category.name);
 
 // Suppression des doublons
 const categoriesSet = [...new Set(categories)];
-console.log(categoriesSet);
 
-// Affichage des catégories en dynamique
+// Affichage des filtres catégories en dynamique
 for (let c = 0; c < categoriesSet.length; c++) {
 
     const sectionCategories = document.querySelector(".categories");
@@ -22,7 +21,7 @@ for (let c = 0; c < categoriesSet.length; c++) {
     const categoriesInput = document.createElement("input");
     categoriesInput.type = "radio";
     categoriesInput.name = "cat";
-    categoriesInput.id = "toggle";
+    categoriesInput.id = categoriesSet[c];
     categoriesInput.hidden = true;
 
     const span = document.createElement("span");
@@ -34,13 +33,59 @@ for (let c = 0; c < categoriesSet.length; c++) {
     sectionCategories.appendChild(categoriesElement);
 }
 
+// Récupération des boutons de filtre en dynamique
+const boutonFiltrer = document.querySelectorAll("input[name='cat']");
+
+// Fonction de filtrage
+let travauxFiltres = travaux;
+
+boutonFiltrer.forEach((bouton) => {
+    bouton.addEventListener("change", () =>{
+        if (bouton.id !== "tous") {
+            travauxFiltres = travaux.filter((travaux) => {
+                return travaux.category.name === bouton.id;
+            });
+        } else {
+            travauxFiltres = travaux;
+            };
+        console.log(travauxFiltres);
+        
+        // Réinitialiser la galerie
+        sectionGallery.innerHTML = "";
+
+        // Afficher les travaux en dynamique
+        for (let i = 0; i < travauxFiltres.length; i++) {
+            const travauxElement = travauxFiltres[i];
+            
+            // Création d’une balise dédiée à un travaux
+            const travauxFigure = document.createElement("figure");
+            travauxFigure.dataset.id = travauxFiltres[i].id;
+            
+            // Création des images
+            const travauxImage = document.createElement("img");
+            travauxImage.src = travauxElement.imageUrl;
+
+            // Ajout de l'attribut alt
+            const travauxAlt = document.createElement("figcaption");
+            travauxAlt.innerText = travauxElement.title;
+
+            // Mise à jour du code dans la section "gallery"
+            sectionGallery.appendChild(travauxFigure);
+            travauxFigure.appendChild(travauxImage);
+            travauxFigure.appendChild(travauxAlt);
+        }
+        }
+    )}
+);
+
+
 // Afficher les travaux en dynamique
-for (let i = 0; i < travaux.length; i++) {
-    const travauxElement = travaux[i];
+for (let i = 0; i < travauxFiltres.length; i++) {
+    const travauxElement = travauxFiltres[i];
     
     // Création d’une balise dédiée à un travaux
     const travauxFigure = document.createElement("figure");
-    travauxFigure.dataset.id = travaux[i].id;
+    travauxFigure.dataset.id = travauxFiltres[i].id;
     
     // Création des images
     const travauxImage = document.createElement("img");
